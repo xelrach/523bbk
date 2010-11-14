e6db3427a299a1941679d33926a09f295ecaf6b4
pbemory <pbemory@gmail.com>
2010-11-13 23:39:54 -0500

    Created different event views for admin versus user

--- bbk_manager/bbk/views.py
+++ bbk_manager/bbk/views.py
@@ -113,6 +113,11 @@ def event_create(request):
 
 def event_details(request, event_id):
     event = get_object_or_404(Event, id=event_id)
+    user = User.objects.get(id=request.session['auth_id'])
+    if request.method=="POST":
+        event.volunteers.add(user)
+        event.save()
+        return HttpResponseRedirect(reverse('bbk.views.events'))
     return render_to_response('event.html', {'event':event})
 
 def event_edit(request, event_id):
@@ -129,6 +134,16 @@ def event_edit(request, event_id):
 	    print e
 	    pass
     return render_to_response('event_edit.html', {'event':event})
+
+def events_admin(request):
+    current_event_list = Event.objects.filter(end__gt=datetime.datetime.now())
+    return render_to_response('events_admin.html', {'current_event_list':current_event_list})
+
+def event_admin_details(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    event_volunteers = event.volunteers.all()
+    length = len(event_volunteers)
+    return render_to_response('event_admin.html', {'event':event, 'event_volunteers':event_volunteers, 'length':length})
 
 def login(request):
     if request.method == 'POST':
@@ -154,8 +169,9 @@ def logout(request):
 def pending(request):
     return render_to_response('pending.html')
 
-def volunteer(request, id):
-    return HttpResponse(str(id))
+def volunteer(request,volunteer_id):
+    volunteer = get_object_or_404(User, id=volunteer_id)
+    return render_to_response('volunteer.html', {'volunteer':volunteer})
 
 def volunteer_signup(request):
     user = User()
